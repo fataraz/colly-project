c2b4d680be50f23f0eb1d30651a497183c1c5af3
razif <fatahillah.razif@gmail.com>
2023-04-02 20:24:12 +0700

    create function crawl

--- main.go
+++ main.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+
+	"github.com/gocolly/colly"
+)
+
 type start struct {
 	Name      string
 	Photo     string
@@ -15,9 +22,24 @@ type movie struct {
 }
 
 func main() {
+	month := flag.Int("month", 1, "Month to fetch birthday for")
+	day := flag.Int("day", 1, "Day to fetch bithdays for")
+	flag.Parse()
 	crawl(*month, *day)
 }
 
 func crawl(month int, day int) {
 
+	c := colly.NewCollector(
+		colly.AllowedDomains("imdb.com", "www.imdb.com"),
+	)
+	infoCollector := c.Clone()
+	c.OnHTML(".mode-detail", func(e *colly.HTMLElement) {
+		profileUrl := e.ChildAttr("div.lister-item-image > a", "href")
+		profileUrl = e.Request.AbsoluteURL(profileUrl)
+		infoCollector.Visit(profileUrl)
+	})
+
+	startUrl := fmt.Sprintf("https://www.imdb.com/search/name/?birth_monthday=%d-%d", month, day)
+	c.Visit(startUrl)
 }
