dec6b9eaa3ba345e3c366ec3d515e50dcc6a4598
razif <fatahillah.razif@gmail.com>
2023-04-03 20:35:41 +0700

    finish project

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
+	"log"
+	"strings"
 
 	"github.com/gocolly/colly"
 )
@@ -38,6 +41,41 @@ func crawl(month int, day int) {
 		profileUrl := e.ChildAttr("div.lister-item-image > a", "href")
 		profileUrl = e.Request.AbsoluteURL(profileUrl)
 		infoCollector.Visit(profileUrl)
+	})
+
+	c.OnHTML("a.lister-page-next", func(e *colly.HTMLElement) {
+		nextPage := e.Request.AbsoluteURL(e.Attr("href"))
+		c.Visit(nextPage)
+	})
+
+	infoCollector.OnHTML("#content-2-wide", func(e *colly.HTMLElement) {
+		tmpProfile := start{}
+		tmpProfile.Name = e.ChildText("h1.header > span.itemprop")
+		tmpProfile.Photo = e.ChildAttr("#name-poster", "src")
+		tmpProfile.JobTitle = e.ChildText("#name-job-categories > a > span.itemprop")
+		tmpProfile.BirthDate = e.ChildAttr("#name-born-info time", "date-time")
+		tmpProfile.Bio = strings.TrimSpace(e.ChildText("#name-bio-text > div.name-trivia-bio-text > div.inline"))
+
+		e.ForEach("div.knownfor-title", func(_ int, kf *colly.HTMLElement) {
+			tmpMovie := movie{}
+			tmpMovie.Title = kf.ChildText("div.knownfor-title-role > a.knownfor-ellipsis")
+			tmpMovie.Year = kf.ChildText("div.knownfor-year > span.knownfor-ellipsis")
+
+			tmpProfile.TopMovies = append(tmpProfile.TopMovies, tmpMovie)
+		})
+		js, err := json.MarshalIndent(tmpProfile, "", "		")
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(string(js))
+	})
+
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("visiting:", r.URL.String())
+	})
+
+	infoCollector.OnRequest(func(r *colly.Request) {
+		fmt.Println("visiting profile URL", r.URL.String())
 	})
 
 	startUrl := fmt.Sprintf("https://www.imdb.com/search/name/?birth_monthday=%d-%d", month, day)
